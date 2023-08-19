9896d5d81b1b1fc5d67dcffc2ecef306f5c3968e
Raj gupta <code.raj11903@gmail.com>
2023-08-19 17:10:37 +0530

    overwrite Condition Handled

--- tic-tac-toe-python/tic_tac_toe.py
+++ tic-tac-toe-python/tic_tac_toe.py
@@ -14,9 +14,16 @@ def move(index,player):
   px =index//3
   py= index%3
   if player == 1:
+    if arr[px][py] =='O':
+      print("Position Marked By Player 2 Previously")
+      return False
     arr[px][py] = 'X'
   else:
+    if arr[px][py] =='X':
+      print("Position Marked By Player 1 Previously")
+      return False
     arr[px][py]= 'O'
+  return True
 
 def u_input(player):
   print("Enter Move for Player {}".format(player))
@@ -44,17 +51,20 @@ def game_status():
 
 
 disp()
-for i in range(0,9):
+i=0
+while i<9:
   if(i%2==0):
     player =1
   else:
     player =2
   index = u_input(player)  # Get the user input for the current player
-  move(index, player)
-  disp()
-  if game_status():
-    print("Player {} Wins".format(player))
-    break
+  if move(index, player):
+    disp()
+    i+=1
+    if game_status():
+        print("Player {} Wins".format(player))
+        break
+
 if game_status()==False:
   print("Game is Draw")
 press = int(input("Enter 1 to play again "))
